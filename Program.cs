cd6e4eb46a8f7b93fb6c90810c9a1b39053508fe
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-08 17:58:37 +0530

    check the stored memory

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GarbageCollectorDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            long mem1 = GC.GetTotalMemory(false);
+            {
+                int[] values = new int[5000];
+            }
+            long mem2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();
+            }
+            long mem3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(mem1);
+                Console.WriteLine(mem2);
+                Console.WriteLine(mem3);
+            }
+        }
+    }
+}
